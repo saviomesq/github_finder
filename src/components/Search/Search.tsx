d9461db1a6507b604c8ded53a4374ac3e51c1740
saviomesq <saviosouzamesquita@gmail.com>
2025-01-06 14:36:39 -0300

    refactor Search component to accept loadUser prop and implement user input handling and implemation requisition API

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,12 +1,22 @@
 import { BsSearch } from "react-icons/bs";
-export const Search = () => {
+import { useState } from "react";
+
+interface SearchProps {
+  loadUser: (userName: string) => Promise<void>;
+}
+export const Search = ({ loadUser }: SearchProps) => {
+  const [userName, setUserName] = useState("");
   return (
     <div>
       <h2>Busque por usuários</h2>
       <p>Conheça seus melhores repositórios</p>
       <div>
-        <input type="text" placeholder="Digite o nome do usuário" />
-        <button>
+        <input
+          type="text"
+          placeholder="Digite o nome do usuário"
+          onChange={(e) => setUserName(e.target.value)}
+        />
+        <button onClick={() => loadUser(userName)}>
           <BsSearch />
         </button>
       </div>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,9 +3,16 @@ import { useState } from "react";
 import { UserProps } from "../types/user";
 export const Home = () => {
   const [user, setUser] = useState<UserProps | null>(null);
+
+  const loadUser = async (userName: string) => {
+    const loadUser = await fetch(`https://api.github.com/users/${userName}`);
+    const data = await loadUser.json();
+    console.log(data);
+    // setUser(data);
+  };
   return (
     <div>
-      <Search />
+      <Search loadUser={loadUser} />
     </div>
   );
 };
